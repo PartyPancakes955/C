42f1b8184a8b3b04bae84c8b9082967d4c26e4c7
PartyPancakes955 <dima22022005@gmail.com>
2019-03-10 21:38:35 +0200

    Add files via upload

---
+++ hello-world.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+void main() { /*
+		printf("Hello, my name's Dima. ");
+		printf("How are you? ");
+		printf("How's the weather?");
+					
+		printf("My name's Dima. ");
+		printf("I'm 13 years old ");
+		printf("And I go to school.");
+						
+		printf("****\n");
+		printf("****\n");
+		printf("****\n");
+		printf("****\n");
+				
+		printf("¯\\_(ツ)_/¯");
+						
+		printf("100 - 76 = %d\n", 100 - 76);	
+		printf("23 * 4 + 11 = %d", 23 * 4 + 11);
+		printf("15 + 7 * 3 = %d\n", 15 + 7 * 3);
+		printf("(12 - 8) * 87 = %d\n", (12 - 8) * 87);
+		printf("(4 - 19) * (3 + 88) - 27 / 3 = %d\n", (4 - 19) * (3 + 88) - 27 / 3);
+									
+										
+		printf("13 + 1 = %d\t\t", 13 + 1);
+		printf("15 %% 5 = %d\t\t", 15 % 5);
+		printf("5 / 2 = %d\n", 5 / 2);
+		printf("18 * 44 = %d\t\t", 18 * 44);
+		printf("100 * 0 = %d\t\t", 100 * 0);
+		printf("78 / 2 = %d\n", 78 / 2);
+		printf("7 - 90 = %d\t\t", 7 - 90);
+		printf("-9 - 3 = %d\t\t", -9 - 3);
+		printf("120 %% 7 = %d\n", 120 % 7);
+		printf("99 + 17 = %d\t\t", 99 + 17);
+		printf("56 - 65 = %d\t\t", 56 - 65);
+		printf("43 / 2 = %d", 43 / 2);
+						
+		printf("*\n");
+		printf("**\n");
+		printf("***\n");
+		printf("****\n");
+		
+		printf("*****\n");
+		printf(" ***\n");
+		printf("  *\n");
+					
+		printf("Till I scarcely more than muttered \"Other friends have flown before--\n");
+		printf("On the morrow he will leave me, as my Hopes have flown before.\"\n");
+		printf(" \tThen the bird said \"Nevermore\".\n");
+		printf(" \t\t\t\t\t\t\t\\ E. A. Poe \\ "); 
+							
+		printf("Square rectangle with side 15\n");
+		printf("S = 15 x 15 = %d", 15 * 15);
+		printf("\nP = (a + a) x 2 = %d", (15 + 15) *2);											
+						
+		printf("12   10 ");
+		printf("\n-- = -- ");
+		printf("\n24   %d",24/12 * 10);
+				       
+		printf("13%% of 300 is %d",300 / 100 * 13);	
+				
+		printf("S = 17 * 18 = %d", 17 * 18);
+		printf("\nP = (17 + 18) * 2 = %d", (17 + 18) * 2);
+					
+		printf("S = (20^2 * 3.14) = %f", 20 * 20 * 3.14 );	
+		printf("\n2PR = 2 * 3.14 * 20 = %f", 2 * 3.14 * 20 );
+							
+		printf("S = (38 / 2 ^2 * 3.14) = %f", 38/2 * 38/2 * 3.14);
+		printf("\n2PR = ( 38 * 3.14) = %f", 38 * 3.14);
+							*/
+							
+							
+	
